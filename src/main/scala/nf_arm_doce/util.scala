5c38598373d772ebd6545cabf68b72d216c35bce
zxhero <zhangxu415@mails.ucas.edu.cn>
2021-12-15 13:17:24 +0800

    Clone fifo from https://github.com/Suedongchu/FIFO-in-Chisel/blob/master/fifo.scala

---
+++ src/main/scala/nf_arm_doce/util.scala
@@ -0,0 +1,55 @@
+package utils
+import chisel3._
+import chisel3.util._
+
+class fifo(val size : Int, val width : Int) extends Module {
+  val io = IO(new Bundle {
+    val dataIn = Input(UInt(width.W))
+    val dataOut = Output(UInt(width.W))
+    val writeFlag = Input(Bool())
+    val readFlag = Input(Bool())
+    val full = Output(Bool())
+    val empty = Output(Bool())
+  })
+
+  val count = RegInit(0.U((log2Ceil(size)+1).W))
+  val mem = Mem(size, UInt(width.W))
+  val wPointer = RegInit(0.U((log2Ceil(size)).W))
+  val rPointer = RegInit(0.U((log2Ceil(size)).W))
+  val dataOut = RegInit(0.U(width.W))
+
+  def indexAdd(index : UInt) : UInt = {
+      Mux(index === (size - 1).U, 0.U, index + 1.U)
+  }
+
+  when(io.writeFlag === true.B && io.readFlag === true.B) {
+    when(count === 0.U) { dataOut := io.dataIn }
+    .otherwise {
+      dataOut := mem(rPointer)
+      rPointer := indexAdd(rPointer)
+      mem(wPointer) := io.dataIn
+      wPointer := indexAdd(wPointer)
+    }
+  } .elsewhen (io.writeFlag === true.B && io.readFlag === false.B) {
+    dataOut := 0.U
+    when(count < size.U) {
+      mem(wPointer) := io.dataIn
+      wPointer := indexAdd(wPointer)
+      count := count + 1.U
+    }
+  } .elsewhen (io.writeFlag === false.B && io.readFlag === true.B) {
+    when(count > 0.U) {
+      dataOut := mem(rPointer)
+      rPointer := indexAdd(rPointer)
+      count := count - 1.U
+    } .otherwise {
+      dataOut := 0.U
+    }
+  } .otherwise {
+    dataOut := 0.U
+  }
+
+  io.dataOut := dataOut
+  io.full := (size.U === count)
+  io.empty := (count === 0.U)
+}
